f1c80d078d6d0b1d0c16d8782a8fbc1dca421f5e
drippydik <drippydik@gmail.com>
2016-08-03 20:38:43 +0100

    Made users a safe place...

--- spec/controllers/volts_controller_spec.rb
+++ spec/controllers/volts_controller_spec.rb
@@ -8,6 +8,15 @@ RSpec.describe VoltsController, type: :controller do
 
             message = "You seem to be looking for things that do not exist - why not try cryptozoology next time?"
             expect(flash[:alert]).to eq message
+        end
+        it "handles permissions errors by redirecting to a safe place" do
+            allow(controller).to receive(:current_user)
 
+            volt = FactoryGirl.create(:volt)
+            get :show, id: volt
+
+            expect(response).to redirect_to(root_path)
+            message = "You aren't allowed to do that."
+            expect(flash[:alert]).to eq message
         end
 end
